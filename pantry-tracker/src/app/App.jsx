a052b30b27069c9e218c61ad63ab742b462e0502
Treasure Mayowa <treasuremayowa@outlook.com>
2024-08-08 21:03:19 +0100

    Remove useState function

--- pantry-tracker/src/app/App.jsx
+++ pantry-tracker/src/app/App.jsx
@@ -1,24 +1,12 @@
 "use client"
 
-import React from "react"; 
-import { useState, useEffect } from "react";
+import React from "react"
 import Footer from './components/Footer'
 import Main from './components/Main'
 import Header from './components/Header'
 
 
 export default function App () {
-    const [items, setItems] = useState([
-        {id: 1, item: "Tomato", quantity: 4, expiry: Date()}
-    ])
-
-    const handleChange = (id, newQuantity) => {
-        setItems(prevItems => 
-            prevItems.map(item => 
-                item.id === id? {...items, quantity: newQuantity}: null
-            )
-        )
-    }
     return (
         <main>
             <Header />
